5e1c6b871cf5a84b1ef2814d152f29f8bced9361
Nick M. Mitchell <nickm@us.ibm.com>
2016-12-23 10:42:01 -0500

    cleanup getAuthToken code a bit

--- actions/getAuthToken/index.js
+++ actions/getAuthToken/index.js
@@ -2,14 +2,11 @@ const ObjectStorage = require('bluemix-objectstorage').ObjectStorage
 
 exports.main = params => {
     return new Promise((resolve, reject) => {
-	console.log("A0")
 	const os = new ObjectStorage(params.creds)
 
 	try {
-	    console.log("A1")
-	    return os.client.refreshToken()
+	    os.client.refreshToken()
 		.then(() => {
-		    console.log("A2", os.client.authToken)
 		    resolve(os.client.authToken)
 		})
 		.catch(reject)
